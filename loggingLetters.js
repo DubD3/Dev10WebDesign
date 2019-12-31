68ac74a5d286e8af1798cbabfc09d61c93b85312
Will.Dorow <will.dorow@gmail.com>
2019-12-31 12:39:30 -0600

    Java script test file

---
+++ loggingLetters.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+var sentence ="I love learning software development"
+
+for(var i=0; i < sentence.length; i++){
+	if(sentence[i] !== " "){
+		console.log(sentence[i]);
+	}
+}
