0b997d97666785edaefd08b2c5f13da05e65f249
Will.Dorow <will.dorow@gmail.com>
2020-01-01 19:29:32 -0600

    Updated formating of lucky seven page and added java script functions rolling dice, tracking current bet,and showing table

--- Lucky Sevens/js/luckySeven.js
+++ Lucky Sevens/js/luckySeven.js
@@ -1,5 +1,38 @@
 // JavaScript Document
-function test() {
-	var jim=0;
-	return jim;
+function main(){
+    var test = document.getElementById("input");
+    var startBet = test.value;
+    var total = startBet;
+    var maxTotal = total;
+    var numOfRounds = 0;
+    alert(startBet);
+    do {
+        var result = roll();
+        if(result ===7){
+            total +=4;
+            if(total > maxTotal){
+                maxTotal = total;
+            }
+        }
+        else{
+            total--;
+        }
+        numOfRounds++;
+    }while(total > 0)
+    console.log("HERE");
+    console.log(numOfRounds);
+    showTable();
 }
+
+function showTable() {
+	var table=document.getElementById("table");
+    table.style.display = "flex";
+}
+function roll(){
+    rolls = rollDice()+rollDice();
+    console.log(rolls);
+    return rolls;
+}
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
