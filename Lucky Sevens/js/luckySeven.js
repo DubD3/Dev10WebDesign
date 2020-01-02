451a9de448e829eb9bfefa67a5542d5c3fa45fa8
Will.Dorow <will.dorow@gmail.com>
2020-01-02 11:19:38 -0600

    Completed basic functionality of the lucky seven game

--- Lucky Sevens/js/luckySeven.js
+++ Lucky Sevens/js/luckySeven.js
@@ -1,61 +1,78 @@
 // JavaScript Document
-function main(){
-    var test = document.getElementById("input");
-    var startBet = test.value;
-    if(!isNaN(startBet)){
-    if(startBet > 0){
-    var total = startBet;
-    var maxTotal = total;
-    var numOfRounds = 0;
-    var maxRoll = 0;
-    alert(startBet);
-    do {
-        numOfRounds++;
-        var result = roll();
-        if(result ===7){
-            total +=4;
-            if(total > maxTotal){
-                maxTotal = total;
-                maxRoll = numOfRounds;
-            }
+function main() {
+    var valuein = document.getElementById("input");
+    var cashBet = valuein.value;
+    
+    if(cashBet.substring(0,1) === "$"){
+         cashBet = cashBet.replace('$' ,' ');
+        alert(cashBet);
+        var startBet =parseFloat(cashBet);
+        //alert(startBet)
+        try {
+         //   alert(startBet)
+            alert(toCurrency(startBet));
+        } catch (error) {
+            alert("Invalid Input")
         }
-        else{
-            total--;
+    }
+    if (!isNaN(startBet)) {
+        if (startBet > 0) {
+            var total = startBet;
+            var maxTotal = total;
+            var numOfRounds = 0;
+            var maxRoll = 0;
+           // alert(startBet);
+            do {
+                numOfRounds++;
+                var result = roll();
+                if (result === 7) {
+                    total += 4;
+                    if (total > maxTotal) {
+                        maxTotal = total;
+                        maxRoll = numOfRounds;
+                    }
+                }
+                else {
+                    total--;
+                }
+
+            } while (total > 0)
+            console.log("HERE");
+            console.log(numOfRounds);
+            var table = document.getElementById("table");
+            //var startBetSpot = document.getElementById("startBet");
+            //startBetSpot.innerText=startBet;
+            addElement("startBet", toCurrency(startBet))
+            addElement("totalRoll", numOfRounds)
+            addElement("highestAmount", toCurrency(maxTotal))
+            addElement("rollCount", maxRoll)
+            showTable(table);
         }
-        
-    }while(total > 0)
-    console.log("HERE");
-    console.log(numOfRounds);
-    var table=document.getElementById("table");
-    //var startBetSpot = document.getElementById("startBet");
-    //startBetSpot.innerText=startBet;
-    addElement("startBet",startBet)
-    addElement("totalRoll",numOfRounds)
-    addElement("highestAmount",maxTotal)
-    addElement("rollCount", maxRoll)
-    showTable(table);
+        else {
+            alert("Bet must be greater than 0");
+        }
     }
-    else{
-        alert("Bet must be greater than 0");
+    else {
+        alert("Bet must be a number")
     }
 }
-else{
-    alert("Bet must be a number")
+function toCurrency(value) {
+    var dollar = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2, });
+    return dollar.format(value);
 }
-}
-function addElement(spot, input){
+function addElement(spot, input) {
     var inputSpot = document.getElementById(spot);
-    inputSpot.innerText=input;
+    inputSpot.innerText = input;
 }
 function showTable(table) {
-	//var table=document.getElementById("table");
+    //var table=document.getElementById("table");
     table.style.display = "flex";
 }
-function roll(){
-    rolls = rollDice()+rollDice();
+function roll() {
+    rolls = rollDice() + rollDice();
     console.log(rolls);
     return rolls;
 }
 function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
-  }
+}
