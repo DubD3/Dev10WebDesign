f3252e8b0b610d687e7e97b37f523a229d3cc2e5
Will.Dorow <will.dorow@gmail.com>
2020-01-01 19:56:58 -0600

    Finished basic functions of the Lucky Seven Application

--- Lucky Sevens/js/luckySeven.js
+++ Lucky Sevens/js/luckySeven.js
@@ -2,30 +2,53 @@
 function main(){
     var test = document.getElementById("input");
     var startBet = test.value;
+    if(!isNaN(startBet)){
+    if(startBet > 0){
     var total = startBet;
     var maxTotal = total;
     var numOfRounds = 0;
+    var maxRoll = 0;
     alert(startBet);
     do {
+        numOfRounds++;
         var result = roll();
         if(result ===7){
             total +=4;
             if(total > maxTotal){
                 maxTotal = total;
+                maxRoll = numOfRounds;
             }
         }
         else{
             total--;
         }
-        numOfRounds++;
+        
     }while(total > 0)
     console.log("HERE");
     console.log(numOfRounds);
-    showTable();
+    var table=document.getElementById("table");
+    //var startBetSpot = document.getElementById("startBet");
+    //startBetSpot.innerText=startBet;
+    addElement("startBet",startBet)
+    addElement("totalRoll",numOfRounds)
+    addElement("highestAmount",maxTotal)
+    addElement("rollCount", maxRoll)
+    showTable(table);
+    }
+    else{
+        alert("Bet must be greater than 0");
+    }
 }
-
-function showTable() {
-	var table=document.getElementById("table");
+else{
+    alert("Bet must be a number")
+}
+}
+function addElement(spot, input){
+    var inputSpot = document.getElementById(spot);
+    inputSpot.innerText=input;
+}
+function showTable(table) {
+	//var table=document.getElementById("table");
     table.style.display = "flex";
 }
 function roll(){
